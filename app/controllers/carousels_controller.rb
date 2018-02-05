bb42bd32246f4c0b92ed716b6a2b44c9141bb431
DC <dachang.h@gmail.com>
2018-02-06 03:25:10 +0800

    added homepage nav

--- app/controllers/carousels_controller.rb
+++ app/controllers/carousels_controller.rb
@@ -3,10 +3,10 @@ class CarouselsController < ApplicationController
 
   def destroy
     @carousel.destroy if @carousel
+    # redirect back
     redirect_back(fallback_location: request.referrer)
     flash[:notice] = "Image Deleted"
   end
-
 
   private
   def find_carousel
