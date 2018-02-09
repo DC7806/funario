51259103fa602dff8567ba9c4761c0ee1c9a4a68
DC <dachang.h@gmail.com>
2018-02-10 03:03:13 +0800

    added PinUploader

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,7 @@ class Pin < ApplicationRecord
   validates :month, presence: true
 
   #carrierwave uploader
-  mount_uploader :image, ImageUploader
+  mount_uploader :image, PinUploader
   
   # [:all_results, :jan, :feb, :mar, :apr, :may, :jun, :jul, :aug, :sep, :oct, :nov, :dec ].each do |month|
   #   mount_uploader month, ImageUploader
